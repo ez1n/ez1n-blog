cd51ee299a7b237a90fac96e913a4c3b528dfa65
unknown <eodrmfdl1004@naver.com>
2023-04-14 14:17:37 +0900

    Style: profile - 반응형 추가

--- components/profile/Avatar/Avatar.jsx
+++ components/profile/Avatar/Avatar.jsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function Avatar() {
   return (
-    <div className={styles.avatar}>
+    <section className={styles.avatar}>
       <Image className={styles.avatarImg} src="/images/profile/profile.jpg" alt="profile image" width={500} height={500}
              priority/>
-    </div>
+    </section>
   )
 }
