59ed521e68ec368aa950a877a14993ee8b5317a9
unknown <eodrmfdl1004@naver.com>
2023-05-12 15:29:34 +0900

    Style: Header, 페이지 변경

--- components/molecules/Navigation/Navigation.jsx
+++ components/molecules/Navigation/Navigation.jsx
@@ -5,9 +5,8 @@ export default function Navigation({isToggle}) {
   return (
     <ul className={`${styles.menu} ${isToggle && `${styles.active}`}`}>
       <li><Link href='/'>Home</Link></li>
-      <li><Link href='/post'>Projects</Link></li>
+      <li><Link href='/post'>Posts</Link></li>
       <li><Link href='/interest'>Interests</Link></li>
-      <li><Link href='/contact'>Contact</Link></li>
     </ul>
   )
 }
