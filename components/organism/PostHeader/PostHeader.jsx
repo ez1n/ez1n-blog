c04c4ac7088910f4f3ea9aa9d8fa88e1d69e64c5
unknown <eodrmfdl1004@naver.com>
2023-05-15 16:29:24 +0900

    Style: PostHeader 버튼 네비게이션 추가

--- components/organism/PostHeader/PostHeader.jsx
+++ components/organism/PostHeader/PostHeader.jsx
@@ -1,4 +1,5 @@
 import styles from "./PostHeader.module.css";
+import Link from "next/link";
 
 import Button from "@/components/atoms/Button/Button";
 
@@ -8,15 +9,15 @@ export default function PostHeader() {
       <h1>Posts</h1>
       <ul className={styles.filter}>
         <li>
-          <Button text={"All"} />
+          <Link href={'/post?filter=all'}><Button text={"All"}/></Link>
         </li>
 
         <li>
-          <Button text={"Projects"} />
+          <Link href={'/post?filter=projects'}><Button text={"Projects"}/></Link>
         </li>
 
         <li>
-          <Button text={"Records"} />
+          <Link href={'/post?filter=records'}><Button text={"Records"}/></Link>
         </li>
       </ul>
     </header>
